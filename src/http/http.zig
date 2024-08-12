facacac4dd5a643907f2790375d12fc49a5a5fd4
Maximilian Dollinger <max@dollinger.dev>
2024-08-12 16:10:29 +0200

    refactor

---
+++ src/http/http.zig
@@ -0,0 +1,2 @@
+pub const Response = @import("./response.zig").Response;
+pub const HttpStatus = @import("./response.zig").HttpStatus;
--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const net = std.net;
 const print = std.debug.print;
 
-const Response = @import("./response.zig");
+const http = @import("./http/http.zig");
 
 pub fn main() !void {
 
@@ -12,19 +12,23 @@ pub fn main() !void {
 
     // Uncomment this block to pass the first stage
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
-    var listener = try address.listen(.{
+    var server = try address.listen(.{
         .reuse_address = true,
     });
-    defer listener.deinit();
+    defer server.deinit();
+
+    var connection = try server.accept();
+    try handleConnection(&connection);
+}
+
+fn handleConnection(connection: *std.net.Server.Connection) !void {
+    print("client connected!\n", .{});
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var connection = try listener.accept();
-    print("client connected!\n", .{});
-
-    var response = Response.Response.init(Response.HttpStatus.OK);
+    var response = http.Response.init(http.HttpStatus.OK);
     const responseBuffer = try allocator.alloc(u8, 1024);
     const answer = try response.serialize(responseBuffer);
     print("sending: {s}", .{answer});
