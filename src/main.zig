73b01bcd4094253899a0115f2a4fa3b339e96b47
Maximilian Dollinger <max@dollinger.dev>
2024-08-12 06:46:51 +0200

    uncommented code

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,20 @@
 const std = @import("std");
 // Uncomment this block to pass the first stage
-// const net = std.net;
+const net = std.net;
+const print = std.debug.print;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
+    print("Logs from your program will appear here!\n", .{});
 
     // Uncomment this block to pass the first stage
-    // const address = try net.Address.resolveIp("127.0.0.1", 4221);
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer listener.deinit();
-    //
-    // _ = try listener.accept();
-    // try stdout.print("client connected!", .{});
+    const address = try net.Address.resolveIp("127.0.0.1", 4221);
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    _ = try listener.accept();
+    print("client connected!", .{});
 }
